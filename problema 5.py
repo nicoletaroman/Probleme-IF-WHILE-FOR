64c9f6d5e4f82b553335f31f754f82d1b7716bc9
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-11-11 11:42:55 +0200

    Add files via upload

---
+++ problema 5.py
@@ -0,0 +1,29 @@
+n=int(input("dati anul "))
+if n%12==11:
+    print("anul oii")
+if n%12==10:
+    print("anul calului")
+if n%12==9:
+    print("anul sarpelui")
+if n%12==8:
+    print("anul dragonului")
+if n%12==7:
+    print("anul iepurelui")
+if n%12==6:
+    print("anul tigrului")
+if n%12==5:
+    print("anul boului")
+if n%12==4:
+    print("anul sobolanului")
+if n%12==3:
+    print("anul porcului")
+if n%12==2:
+    print("anul cainelui")
+if n%12==1:
+    print("anul cocosului")
+if n%12==0:
+    print("anul maimutei")
+
+
+
+
